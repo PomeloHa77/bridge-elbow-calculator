d1e1a22736f9712436680c517d9e5108a032cd96
POMELO <PomeloHa77@outlook.com>
2026-06-24 16:37:13 +0800

    feat: add Home and Special entry pages

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <van-nav-bar title="计算" />
+    <div class="cards">
+      <van-cell-group inset>
+        <van-cell title="万能爬坡" label="上下弯 · 计算斜边长和切口尺寸" is-link @click="$router.push('/climb')" size="large" />
+        <van-cell title="水平弯" label="左右弯 · 计算斜边长和切口尺寸" is-link @click="$router.push('/horizontal')" size="large" />
+      </van-cell-group>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.page { padding-bottom: 60px; }
+.cards { margin-top: 16px; }
+</style>
---
+++ src/views/Special.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <van-nav-bar title="特殊计算" />
+    <div class="cards">
+      <van-cell-group inset>
+        <van-cell title="多层过桥" label="多层排布 · 起弯点错位计算" is-link @click="$router.push('/multilayer')" size="large" />
+        <van-cell title="大小头变径" label="偏心/同心变径 · 对角切割长度" is-link @click="$router.push('/reducer')" size="large" />
+        <van-cell title="死角弯避让" label="障碍物避让 · 提前起弯距离" is-link @click="$router.push('/avoidance')" size="large" />
+      </van-cell-group>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.page { padding-bottom: 60px; }
+.cards { margin-top: 16px; }
+</style>
